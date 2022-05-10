13465e7cf9e306dfbab8c07ba44140f9fb52bf0a
fxrxbx <fungairbobo@gmail.com>
2022-05-10 19:50:20 +0200

    qa-automation-lslcrp

---
+++ standard_user Login.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+import time
+
+driver: WebDriver = webdriver.Chrome('/Users/fxrxbx/Downloads/chromedriver')
+
+driver.get("https://qa-challenge.codesubmit.io/")
+
+element = driver.find_element(By.ID, "user-name")
+element.clear()
+element.send_keys("standard_user")
+time.sleep(1)
+
+element = driver.find_element(By.ID, "password")
+element.clear()
+element.send_keys("secret_sauce")
+time.sleep(1)
+
+element = driver.find_element(By.ID, "login-button")
+element.click()
