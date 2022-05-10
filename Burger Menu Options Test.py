b0827c17f66a9b05874d3c260efc7a4226109119
fxrxbx <fungairbobo@gmail.com>
2022-05-10 20:23:44 +0200

    qa-automation-lslcrp

--- Burger Menu Options Test.py
+++ Burger Menu Options Test.py
@@ -61,6 +61,17 @@ element = driver.find_element(By.ID, "inventory_sidebar_link")
 element.click()
 time.sleep(1)
 
+element = driver.find_element(By.ID, "item_5_img_link")
+element.click()
+time.sleep(1)
+element = driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket")
+element.click()
+time.sleep(1)
+
+element = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+element.click()
+time.sleep(1)
+
 element = driver.find_element(By.ID, "react-burger-menu-btn")
 element.click()
 time.sleep(1)
@@ -71,10 +82,12 @@ time.sleep(1)
 
 element = driver.find_element(By.ID, "logout_sidebar_link")
 element.click()
+time.sleep(1)
 
 element = driver.find_element(By.ID, "user-name")
 element.clear()
 element.send_keys("standard_user")
+time.sleep(1)
 
 element = driver.find_element(By.ID, "password")
 element.clear()
